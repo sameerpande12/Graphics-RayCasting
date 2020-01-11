861347de1fcba06ffc3813dd9fe15cbc260b77d5
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-01-12 01:13:41 +0530

    intersection functions

--- Plane.cpp
+++ Plane.cpp
@@ -1,6 +1,23 @@
 #include "Plane.h"
-
+#include<glm/glm.hpp>
 Plane::Plane(glm::dvec3 norm,double dval){
     normal = normal;
     d = dval;
 }
+
+std::vector<glm::dvec3> Plane::getIntersections(Ray ray){
+    std::vector<glm::dvec3> intersections;
+    double normal_dir_dot_product =glm::dot(normal,ray.getDirection());
+    if(  normal_dir_dot_product== 0.0){
+        if( glm::dot(normal,ray.getOrigin())==d){
+            intersections.push_back(ray.getOrigin());
+        }   
+        
+    }
+    else{
+        double t = (d - glm::dot(normal,ray.getOrigin())) /normal_dir_dot_product;
+        intersections.push_back(ray.scale(t));
+    }
+    return intersections;
+
+};
--- Plane.h
+++ Plane.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<glm/glm.hpp>
+#include "Ray.h"
+#include <vector>
 
 class Plane{
     public:
@@ -7,4 +9,6 @@ class Plane{
         double d;
         //equation of the plane will be glm::dot(r,normal) = d;
         Plane(glm::dvec3 normal,double d);
+
+        std::vector<glm::dvec3> getIntersections(Ray r);
 };

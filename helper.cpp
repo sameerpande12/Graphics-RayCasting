081d18fb37e7e92314f3d908961bcf0309c0ae13
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-01-13 23:54:39 +0530

    added function for rotation of axes and vectors about x,y,z axes

--- helper.cpp
+++ helper.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <math.h>
 #include"helper.h"
+#include "Axes.h"
 
 std::vector<double> solveQuadratic(double a, double b, double c){
     std::vector<double>solutions;
@@ -17,5 +18,46 @@ std::vector<double> solveQuadratic(double a, double b, double c){
         solutions.push_back( (-b - sqrt(discriminant)  )/(2*a) );
     }
     return solutions;
+
+}
+
+
+Axes rotate(Axes input,double angle, int axisID){
+    Axes output = Axes();
+    output.x = rotateVector(input.x,angle,0);
+    output.y = rotateVector(input.y,angle,1);
+    output.z = rotateVector(input.z,angle,2);
+    return output;
+    
+};
+
+glm::dvec3 rotateVector(glm::dvec3 input,double angle, int axisID){
+
+    glm::dmat3 rotation;
+ 
+    if(axisID%3==0){
+
+        rotation = glm::dmat3(  1,          0,          0,
+                                0, cos(angle),-sin(angle),
+                                0, sin(angle),cos(angle) );
+    }
+    else if(axisID%3==1){
+
+        rotation = glm::dmat3(  cos(angle),          0,          sin(angle),
+                                0,          1,          0,
+                                -sin(angle),0, cos(angle) );
+
+    }
+
+    else{
+        rotation = glm::dmat3(
+            cos(angle), -sin(angle),0,
+            sin(angle),cos(angle),0,
+            0,0,1
+        );
+
+    }
+
+    return rotation * input;
 
 }
--- helper.h
+++ helper.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <math.h>
+#include "GL/glew.h"
+
 
 struct Color{
     GLubyte red;
@@ -9,3 +11,6 @@ struct Color{
     GLubyte blue;
 };
 std::vector<double> solveQuadratic(double a, double b, double c);
+
+Axes rotate(Axes input,double angle, int axisID);//rotate axes by an angle about axisID (0 for x, 1 for y , 2 for z)
+glm::dvec3 rotateVector(glm::dvec3 input,double angle, int axisID);//rotate by an angle about axis with axisID(0 for x, 1 for y, 2 for z)

7f65dab1b599e1e98d2bc6ad6d85af39d7c0570e
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-01-13 23:07:30 +0530

    Camera pixel to world conversion setup done

---
+++ Camera.cpp
@@ -0,0 +1,44 @@
+#include "Camera.h"
+
+Camera::Camera(glm::dvec3 loc, Axes ax, int w, int h, double FOV){
+    location = loc;
+    axes = ax;
+    width = w;
+    height = h;
+    fov = FOV;
+    aspectRatio = width/height;
+    
+}
+
+glm::dvec3 Camera::cameraToWorld(glm::dvec3 input){
+
+    glm::dvec3 output = glm::dvec3(0,0,0);
+
+    for(int i =0 ;i<3;i++){
+        for(int j=0;j<3;j++){
+            output[i]= output[i] + axes.getAxis(j)[i] * input[j];
+        }
+    }
+
+    for(int i=0;i<3;i++)output[i] += location[i];
+
+
+    return output;
+}
+
+glm::dvec3 Camera::pixelToCamera(double px,double py){
+    
+    
+    double pixelCamerax = (2* (px+0.5)/width - 1)  * aspectRatio * tan(fov/2);
+    double pixelCameray = (2* (py + 0.5)/height - 1) * tan(fov/2);
+
+    return glm::dvec3( pixelCamerax,pixelCameray,-1);
+
+
+
+}
+
+glm::dvec3 Camera::pixelToWorld(double px,double py){
+    
+    return  cameraToWorld(pixelToCamera(px,py));
+}
---
+++ Camera.h
@@ -0,0 +1,41 @@
+#pragma once
+#include<math.h>
+#include "Axes.h"
+#include<glm/glm.hpp>
+class Camera{
+    private:
+        double aspectRatio;
+
+    public:
+        
+
+        glm::dvec3 location;
+        Axes axes;
+        int width;
+        int height;
+        double fov = M_PI_2;// pi/2
+        Camera(glm::dvec3 location,Axes axes,int width,int height,double fov);
+
+        glm::dvec3 cameraToWorld(glm::dvec3 input);
+        glm::dvec3 pixelToCamera(double i, double j);//i,j are pixel numbers. ought to be integers but we have kept it as double for the ease of computation
+
+        glm::dvec3 pixelToWorld(double i,double j); 
+
+        void moveCamera(glm::dvec3 displacement);
+
+
+        // screen follow coordinate system
+    /*                 
+                        y
+                        ^
+                        |
+                        |
+                        |(0,0)
+                        ------------> x
+                        width is along x
+                        height is along y
+        */  
+        
+
+
+};
